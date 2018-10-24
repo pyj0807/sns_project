7dc3403f73efc6d7b628edead507d15f1c53465e
joon <joon@joon-PC>
2018-10-24 14:10:10 +0900

    commit 1024 0210

--- src/sns/controller/JoinController.java
+++ src/sns/controller/JoinController.java
@@ -6,8 +6,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class JoinController {
 	
+	
+	
 	@RequestMapping("/join.do")
 	public String joinHandle() {
+		
+		
+		
+		
+		
 		return "index/join";
 	}
 }
